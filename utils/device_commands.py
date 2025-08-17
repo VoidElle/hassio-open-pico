535ff757651be05ce084c34513e0b4656caca16e
VoidElle <lucadelc@gmail.com>
2025-08-17 19:48:58 +0200

    Refactor get_on_off_command function to use new_value parameter for clarity

--- utils/device_commands.py
+++ utils/device_commands.py
@@ -1,10 +1,9 @@
 # Function to retrieve the command to set ON / OFF to a device
-def get_on_off_command(is_device_on: bool, device_pin: str):
+def get_on_off_command(new_value: bool, device_pin: str):
     return {
-        "on_off": 1 if is_device_on else 2,
+        "on_off": 1 if new_value else 2,
         "cmd": "upd_pico",
         "frm": "mqtt",
-        "idp": 2 if is_device_on else 1,
+        "idp": 2 if new_value else 1,
         "pin": device_pin
     }
-    return f"{{\"on_off\":{1 if is_device_on else 2},\"cmd\":\"upd_pico\",\"frm\":\"mqtt\",\"idp\":{2 if is_device_on else 1},\"pin\":\"{device_pin}\"}}"
