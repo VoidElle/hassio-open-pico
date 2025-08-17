df9e6f271e811e61c740efef63d9e46a336b9456
VoidElle <lucadelc@gmail.com>
2025-08-17 21:23:34 +0200

    Add speed control functionality to fan device and refactor related methods

--- utils/device_commands.py
+++ utils/device_commands.py
@@ -8,10 +8,22 @@ def get_on_off_command(new_value: bool, device_pin: str):
         "pin": device_pin
     }
 
+# Function to retrieve the command to change the mode to a device
 def get_change_mode_command(new_mode: int, device_pin: str):
     return {
         "mod": new_mode,
         "on_off": 1,
+        "cmd": "upd_pico",
+        "frm": "mqtt",
+        "idp": 1,
+        "pin": device_pin
+    }
+
+# Function to retrieve the command to change the speed of the device
+def get_change_speed_command(percentage: int, device_pin: str):
+    return {
+        "spd_row": percentage,
+        "speed": 0,
         "cmd": "upd_pico",
         "frm": "mqtt",
         "idp": 1,
